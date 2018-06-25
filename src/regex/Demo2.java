f713ec3f6e6f815691945bae1f161146491f7f96
sty0621 <353269916@qq.com>
2018-06-25 18:19:43 +0800

    sty0621

---
+++ src/regex/Demo2.java
@@ -0,0 +1,29 @@
+package regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Demo2 {
+	public static void main(String[] args) {
+		//在这个字符串中：asfsd23323，是否符合正则表达式：\w+
+		
+		//表达式对象
+		Pattern p = Pattern.compile("\\w+");
+		
+		//创建Matcher对象
+		Matcher m = p.matcher("asfsd23323");
+		
+		boolean yn = m.matches();//尝试将整个整个序列与该模式匹配
+		System.out.println(m.group());
+		System.out.println(m.group(0));
+		boolean yn2 = m.find();//该方法扫描输入序列，查找与该模式匹配的下一个子序列
+		
+
+		
+		boolean yn3 = m.find();
+		
+		System.out.println(yn);
+		System.out.println(yn2);
+		
+	}
+}
---
+++ src/regex/WebSpiderTest.java
@@ -0,0 +1,58 @@
+package regex;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 网络爬虫取链接
+ * @author 南风添
+ *
+ */
+public class WebSpiderTest {
+	
+	public static String getURLContent(String urlStr, String charsetName) {
+		StringBuilder sb = new StringBuilder();
+		try {
+			URL url = new URL(urlStr);
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), Charset.forName(charsetName)));
+			
+			String temp = "";
+			while((temp = reader.readLine()) != null) {
+				sb.append(temp);
+			}
+			
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		String destStr = getURLContent("http://www.163.com", "gbk");
+		
+		System.out.println(destStr);
+		
+		Pattern p1 = Pattern.compile("<a[\\s\\S]+?</a>");
+		
+		Pattern p2 = Pattern.compile("href=\"(.+?)\"");
+		
+		Matcher m = p2.matcher(destStr);
+		
+		while(m.find()) {
+			System.out.println(m.group(1));
+		}
+		
+	}
+}
